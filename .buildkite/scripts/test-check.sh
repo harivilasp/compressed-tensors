20945454c3d618d53355fe542c28cc97d1aa9f5f
Dan Huang <dahuang@redhat.com>
2026-05-13 15:55:17 -0400

    install compilers and run tests
    Signed-off-by: Dan Huang <dahuang@redhat.com>

--- .buildkite/scripts/test-check.sh
+++ .buildkite/scripts/test-check.sh
@@ -2,10 +2,17 @@
 
 cat /etc/issue
 
-# install uv and check env
-apk add --no-cache curl
+# install uv/compilers and check env
+apk add --no-cache curl build-base
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export LD_LIBRARY_PATH=/usr/local/nvidia/lib64
 export PATH="$HOME/.local/bin:/usr/local/nvidia/bin:$PATH"
 nvidia-smi
 uv --version
+
+# install code and run tests
+uv venv testvenv --python 3.10
+source testvenv/bin/activate
+
+uv pip install .[dev] --extra-index-url https://download.pytorch.org/whl/cu129
+make test
